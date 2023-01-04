908d018ce2821b163aafff294846e9e219fde1a6
patelshivani3 <shivpatel3001@gmail.com>
2023-01-04 16:52:34 +0530

    Ceate contact details of a person

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -10,11 +10,48 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWelcome to Address Book Programs");
-           //UC1 Create a contact deatails of person
-            Person.AddPerson();
-            Person.PrintPerson();
+            Console.WriteLine("\nWelcome To Address Book Program");
+            Console.WriteLine("\n1.Create Contact\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                //UC1 - Create Contact
+                case 1:
+                    CreateContacts();
+                    break;
+                default:
+                    Console.WriteLine("\nChoose from given option only!\n");
+                    break;
+            }
             Console.ReadLine();
+        }
+        public static void CreateContacts()
+        {
+            //reading data and storing in set method/properties 
+            Console.Write("Enter First Name : ");
+            string firstName = Console.ReadLine();
+            
+            Console.Write("Enter Last Name : ");
+            string lastName = Console.ReadLine();
+ 
+            Console.Write("Enter Address : ");
+            string address = Console.ReadLine();
+            
+            Console.Write("Enter City Name : ");
+            string city = Console.ReadLine();
+            
+            Console.Write("Enter State Name : ");
+            string state = Console.ReadLine();
+          
+            Console.Write("Enter ZIP Code : ");
+            int ZIPCode = Convert.ToInt32(Console.ReadLine());
+           
+            Console.Write("Enter Phone Number : ");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+          
+            Console.Write("Enter Email Id : ");
+            string email = Console.ReadLine();
         }
     }
 }
+
